28d3ffd631def93f650db6539e421085c003a81a
Zoe Xanos <zxanos@gmail.com>
2020-05-08 04:43:42 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,5 @@ first_number = 6
 second_number = 56
 sum = first_number + second_number
 
-#contains a local variable called product that is assigned to the #result of multiplying first_number and second_number
+#contains a local variable called product that is assigned to the result of multiplying first_number and second_number
+#contains a local variable called quotient that is assigned to the result of dividing first_number by second_number
