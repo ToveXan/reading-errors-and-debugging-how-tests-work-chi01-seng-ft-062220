1a64317de287eb25b0789868d5d5666c3328cf8b
Zoe Xanos <zxanos@gmail.com>
2020-05-08 04:43:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,5 @@
 first_number = 6
 second_number = 56
 sum = first_number + second_number
+
+#contains a local variable called product that is assigned to the #result of multiplying first_number and second_number
